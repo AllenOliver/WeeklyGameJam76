a0638996e6fedfb6823dbebed8d2edd7f3ab0bb9
Allen Oliver <32452527+AllenOliver@users.noreply.github.com>
2018-12-22 13:40:11 -0500

    Added core of project with Player Scene and a Global Singleton
    Player scene with a spot for animations.
    
    Global singleton to add flags and funtions to.
    
    Updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# WeeklyGameJam76
+# WeeklyGameJam76 - Theme "Afterlife"
+
+## Engine
+- Godot
+
+---
+### Tri-Cities Game Developers - 2018
---
+++ WeeklyGameJam76/Globals.gd
@@ -0,0 +1,4 @@
+#Globals - Global functions and vars to call anywhere.
+## Singleton, will be called before anything else!
+extends Node2D
+
